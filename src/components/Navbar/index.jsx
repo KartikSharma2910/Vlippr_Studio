492f4a5b46d8dde8e9fd4e900820484b08e93dcd
Kartik Sharma <kartik@losung360.com>
2024-03-27 17:06:30 +0530

    Drawer Changes

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,17 @@
-import { Box } from "@mui/material";
+import DragHandleSharpIcon from "@mui/icons-material/DragHandleSharp";
+import FacebookSharpIcon from "@mui/icons-material/FacebookSharp";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import { Box, Drawer, IconButton, Slide } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { navbar } from "../../constants";
+import { useResponsive } from "../../hooks/useResponsive";
 import styles from "./styles";
 
 const Navbar = () => {
   const [isScrolled, setIsScrolled] = useState(false);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const { screenType } = useResponsive();
 
   useEffect(() => {
     window.addEventListener("scroll", handleScroll);
@@ -21,7 +28,13 @@ const Navbar = () => {
     }
   };
 
-  console.log(isScrolled);
+  const toggleDrawer = () => {
+    setIsDrawerOpen(!isDrawerOpen);
+  };
+
+  const handleDrawerClose = () => {
+    setIsDrawerOpen(false);
+  };
 
   return (
     <Box
@@ -41,23 +54,66 @@ const Navbar = () => {
           src="https://s3-alpha-sig.figma.com/img/2fb2/04bf/2f6fdad30e09c745c239d3a394367025?Expires=1712534400&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=PEnBACeEJmS6pcBADmN5lsMeATapEO64z8Vh6q1rHUAm3tYrw3JCss7bfjkTztycADYVzfwNWVokP18mVbJSaBasU9d84pW~RRnT4fE0HGBlE1rZJ4kErke0J6mmH4Ubaf~xSL2L~HJkBByCB53yXBDErQ4IFR0HoiVduUn8qslNd~moXpRNgBa7D-frSZFTqg~E93RfW1GrQiVPrpOqizAQtN6dgJ9ToVH73g6cCNRH7e9j27nT8IhnP4sXLorBwOsRpHDowN-zw6u~q-O52FKXkIlRPOVCQJBuKkgpcCw0UjeO3Aggy30MvWxgfiynuqTk4awEOvN42h8Pwjljow__"
         />
       </Box>
-      <Box
-        sx={{
-          ...styles.listContainer,
-          borderColor: isScrolled ? "black" : "white",
-        }}
-      >
-        {navbar.map(({ label, link }, index) => (
-          <Box
-            key={index}
-            component="a"
-            href={link}
-            sx={{ ...styles.list, color: isScrolled ? "black" : "white" }}
+
+      {screenType === "TABLET" || screenType === "MOBILE" ? (
+        <Box>
+          <IconButton onClick={toggleDrawer}>
+            <DragHandleSharpIcon
+              sx={{ color: isScrolled ? "black" : "white" }}
+            />
+          </IconButton>
+          <Drawer
+            anchor="right"
+            open={isDrawerOpen}
+            onClose={handleDrawerClose}
+            elevation={5}
+            PaperProps={{ variant: "outlined" }}
+            TransitionComponent={Slide}
+            transitionDuration={{ enter: 300, exit: 300 }}
+            sx={styles.drawer}
           >
-            {label}
-          </Box>
-        ))}
-      </Box>
+            <Box sx={styles.drawerContent}>
+              <Box sx={styles.drawerHeads}>Menu</Box>
+              <Box sx={styles.drawerItemWrapper} onClick={handleDrawerClose}>
+                {navbar.map(({ label, link }, index) => (
+                  <Box
+                    key={index}
+                    component="a"
+                    href={link}
+                    sx={styles.drawerItem}
+                  >
+                    {label}
+                  </Box>
+                ))}
+              </Box>
+              <Box sx={styles.drawerHeads}>Social</Box>
+              <Box sx={styles.iconWrapper}>
+                <FacebookSharpIcon sx={styles.icon} />
+                <LinkedInIcon sx={styles.icon} />
+                <TwitterIcon sx={styles.icon} />
+              </Box>
+            </Box>
+          </Drawer>
+        </Box>
+      ) : (
+        <Box
+          sx={{
+            ...styles.listContainer,
+            borderColor: isScrolled ? "black" : "white",
+          }}
+        >
+          {navbar.map(({ label, link }, index) => (
+            <Box
+              key={index}
+              component="a"
+              href={link}
+              sx={{ ...styles.list, color: isScrolled ? "black" : "white" }}
+            >
+              {label}
+            </Box>
+          ))}
+        </Box>
+      )}
     </Box>
   );
 };
--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -7,6 +7,7 @@ const styles = {
     position: "sticky",
     top: 0,
     zIndex: 100,
+    height: "65px",
     padding: "15px 60px",
     transition: "all 400ms",
   },
@@ -24,8 +25,71 @@ const styles = {
   },
 
   list: {
-    color: "white",
+    color: "black",
     fontSize: "14px",
+  },
+
+  drawer: {
+    "& .MuiDrawer-paper": {
+      zIndex: 100,
+      position: "fixed",
+      width: {
+        xs: "250px",
+        sm: "270px",
+      },
+      opacity: "0.9",
+      maxWidth: {
+        xs: "300px",
+        sm: "340px",
+      },
+      borderRadius: 0,
+      borderTop: "none",
+      borderBottom: "none",
+      padding: "20px",
+      top: 65,
+      backgroundColor: "white",
+      height: `calc(100% - 65px)`,
+    },
+  },
+
+  drawerContent: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "2rem",
+  },
+
+  drawerHeads: {
+    fontSize: "14px",
+    userSelect: "none",
+  },
+
+  iconWrapper: {
+    display: "flex",
+    gap: "1rem",
+    alignItems: "center",
+  },
+
+  icon: { fontSize: "30px", cursor: "pointer" },
+
+  drawerItemWrapper: {
+    display: "flex",
+    flexDirection: "column",
+  },
+
+  drawerItem: {
+    color: "black",
+    padding: "20px 0px 20px 30px",
+    fontSize: {
+      xs: "14px",
+      sm: "16px",
+    },
+    fontWeight: 500,
+    borderBottom: "1px solid #8C8C8C",
+    transition: "all 400ms",
+
+    "&:hover": {
+      textDecoration: "underline",
+    },
   },
 };
 
